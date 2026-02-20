23f055d48a734e9d7fedec8d00990cf59afa13b7
Hayden-Albrecht <hayden.albrecht@colostate.edu>
2026-02-20 09:41:28 -0700

    Update checker.c

--- HW2/checker.c
+++ HW2/checker.c
@@ -8,12 +8,12 @@ int main(int argc, char *argv[]){
 int Arg1_PlaceHolder = atoi(argv[1]); // making string args into integers
 int Arg2_PlaceHolder = atoi(argv[2]);
     printf ("Checker Process [%d]: Starting.\n", getpid()); //starting process
-if (Arg2_PlaceHolder % Arg1_PlaceHolder == 0){ //checking divisibility
+if (Arg1_PlaceHolder % Arg2_PlaceHolder == 0){ //checking divisibility
     printf("Checker Process [%d]: %d *IS* divisible by %d.\n", getpid(), Arg1_PlaceHolder, Arg2_PlaceHolder);
     printf("Checker Process [%d]: Returning 1.\n", getpid());
     return 1;
 
-} else if(Arg2_PlaceHolder % Arg1_PlaceHolder !=0){ // continuing to check divisibility
+} else if(Arg1_PlaceHolder % Arg2_PlaceHolder !=0){ // continuing to check divisibility
     printf("Checker Process [%d]: %d *IS NOT* divisible by %d.\n",getpid(), Arg1_PlaceHolder, Arg2_PlaceHolder);
     printf("Checker Process [%d]: Returning 0.\n", getpid());
     return 0;
